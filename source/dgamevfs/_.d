2b0e8732e19d3aeec0f621b2393033585d9cefd9
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-05-13 17:06:15 +0200

    Recovered _.d module but marked as deprecated

---
+++ source/dgamevfs/_.d
@@ -0,0 +1,14 @@
+
+//          Copyright Ferdinand Majerech 2011 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+deprecated("Use dgamevfs")
+module dgamevfs._;
+
+public import dgamevfs.exceptions;
+public import dgamevfs.fs;
+public import dgamevfs.memory;
+public import dgamevfs.stack;
+public import dgamevfs.vfs;
