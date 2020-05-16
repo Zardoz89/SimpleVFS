a82f55c1107158ecbe6e2b6efea3994c004523df
Luis Panadero Guardeño <luis.panadero@gmail.com>
2020-05-16 10:19:13 +0200

    Update autogenerated docs

--- source/simplevfs/vfs.d
+++ source/simplevfs/vfs.d
@@ -4,7 +4,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-/// Base classes and structs defining the general API of D:GameVFS.
+/// Base classes and structs defining the general API of SimpleVFS.
 module simplevfs.vfs;
 
 import std.container;
